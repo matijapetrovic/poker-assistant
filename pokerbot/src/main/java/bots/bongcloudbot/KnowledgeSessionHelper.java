e2dd2a56dbfddfc4bedf6c6e235ee40721660f11
nikolakabasaj <44526113+nikolakabasaj@users.noreply.github.com>
2021-06-03 19:04:57 +0200

    Preflop and postflop tests have started

---
+++ pokerbot/src/main/java/bots/bongcloudbot/KnowledgeSessionHelper.java
@@ -0,0 +1,27 @@
+package bots.bongcloudbot;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+
+public class KnowledgeSessionHelper {
+	
+	public static KieContainer createRuleBase() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kieContainer = ks.getKieClasspathContainer();
+		return kieContainer;
+	}
+	
+	public static StatelessKieSession getStatlessKnowledgeSession(KieContainer kieContainer, String sessionName) {
+		StatelessKieSession kSession = kieContainer.newStatelessKieSession(sessionName);
+		return kSession;
+	}
+	
+	public static KieSession getStatefulKnowledgeSession(KieContainer kieContainer, String sessionName) {
+		KieSession kSession = kieContainer.newKieSession(sessionName);
+		return kSession;
+	}
+
+
+}
